40f83ec12729cc027d0581ef8b43608fada64d36
jagadee93 <jagadeeshg1000@gmail.com>
2024-01-07 23:58:47 +0530

    waitlist

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,7 +1,7 @@
 const allowedOrigins = require('./allowedOrigins');
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    if (allowedOrigins.indexOf(origin) !== -1) {
       // origin in allowed origin  || no origin //TODO:Remove no origin field in production
       callback(null, true); //error, allowed
     } else {
